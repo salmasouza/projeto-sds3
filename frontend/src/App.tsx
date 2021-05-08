83dbf32aa2e8049d3daee6145c68bcd3f2004e0f
Salma Souza <salma.souzza@gmail.com>
2021-05-08 20:39:12 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,11 @@
 
 
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import NavBar from "components/NavBar";
-import Footer from "components/Footer";
+
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary">Dashboard de Vendas</h1>
-
-        <div className='row px-3'>
-          <div className="col-sm-6">
-            <h5>Todas as Vendas</h5>
-            <BarChart/>
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secundary">Todas as Vendas</h5>
-            <DonutChart/>
-          </div>
-          
-
-          
-
-        <div className="py-3">
-          <h2 className="twxt-primary">Todas as Vendas</h2>
-        </div>
-
-        </div>
-
-
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Página Home</h1>
+        </div>
+    );
+}
+  
+  export default Home;
+  
+
+
